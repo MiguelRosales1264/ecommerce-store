1637d26473bc798a897ed4ff401132f57f08da41
MiguelRosales1264 <miguelrosales1264@gmail.com>
2025-06-21 06:47:27 -0600

    update

---
+++ ecommerce/src/components/CheckoutForm.tsx
@@ -0,0 +1,7 @@
+interface CheckoutFormProps {
+    onSubmit: (body: JSON) => void;
+}
+
+export default function ChekcoutForm({ onSubmit }: CheckoutFormProps) {
+    
+}
--- ecommerce/src/pages/Checkout.tsx
+++ ecommerce/src/pages/Checkout.tsx
@@ -1,6 +1,7 @@
 import { use } from "react";
 import { CartContext } from "../context/CartContext"
 import CartSummary from "../components/CartSummary";
+import CheckoutForm from "../components/CheckoutForm";
 
 export default function Checkout() {
     const { items, cartTotal } = use(CartContext);
@@ -23,7 +24,7 @@ export default function Checkout() {
         <div className="p-8 max-w-4xl mx-auto">
             <h1 className="text-2xl font-bold mb-6">Checkout</h1>
             <div className="grid md:grid-cols-2 gap-8">
-                {/* <CheckoutForm onSubmit={handleCheckout} /> */}
+                <CheckoutForm onSubmit={handleCheckout} />
                 <CartSummary items={items} cartTotal={cartTotal} />
             </div>
         </div>
